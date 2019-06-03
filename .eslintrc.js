704a26cee0e11d99333a197c5bfac253d1a3c85e
Johannes Ferber <jf@blackdackel.com>
2019-06-03 19:33:21 +0200

    Removed package-lock.json and added linting

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+	"parserOptions": {
+		"ecmaVersion": 2015
+	},
+	"rules": {
+		"linebreak-style": [
+            "error",
+            "unix"
+        ],
+		"semi": [
+            "error",
+            "always"
+        ]
+	},
+	"env": {
+		"node": true,
+		"mocha": true
+	}
+};
